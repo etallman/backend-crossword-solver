4a438dd1fbc5e4f3744fa9ed22653842d01bd476
Eileen <eileen.tallman@gmail.com>
2019-06-13 10:15:32 -0400

    Main function completed

--- xword.py
+++ xword.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
 
-__author__ = "???"
+__author__ = "Eileen"
 
 # YOUR HELPER FUNCTION GOES HERE
+
+import re
 
 
 def main():
@@ -13,6 +15,11 @@ def main():
 
     test_word = raw_input(
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
+    regex_word = test_word.replace(' ', '\\w')
+    result = [
+        word for word in words if len(test_word) == len(word) and re.match(regex_word, word)
+    ]
+    print(result)
 
     # YOUR ADDITIONAL CODE HERE
     raise NotImplementedError('Please complete this')
